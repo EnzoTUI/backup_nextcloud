892f1d097fda5fe7a35ee581773a7027763ff38d
Enzo <etuihagi@gmail.com>
2019-02-22 17:08:26 +0000

    added restore.sh

---
+++ backup_restore.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+MYSQL_HOST=localhost
+MYSQL_USER=root
+MYSQL_PASS=root
+MYSQL_DBNAME=nextcloud
+
+echo "Entrez la date de restauration voulu"
+read date
+
+# Arrêt du service Nextcloud sur le serveur
+ssh root@192.168.33.200 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on'
+
+# Verrouillage de la snapshot
+zfs hold keep data/backup@nextcloud_$date
+
+# Clone de la snapshot
+zfs clone date/backup@nextcloud_$date data/restore
+
+# Restauration des fichiers du serveur NextCloud
+rsync -Aavx /data/restore/nextcloud-data/ -e "ssh" root@192.168.33.200:/var/www/html/nextcloud/
+# Nettoyage de la BDD avant restauration
+ssh root@192.168.33.200  "mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASS -e \"DROP DATABASE $MYSQL_DBNAME\""
+ssh root@192.168.33.200  "mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASS -e \"CREATE DATABASE $MYSQL_DBNAME\""
+
+# Restauration de la BDD du serveur NextCloud
+ssh root@192.168.33.200 "mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DBNAME" < /data/restore/nextcloud-bdd.bak
+
+# Suppression du clone de la snapshot
+zfs destroy data/restore
+# Déverrouillage de la snapshot
+zfs release keep data/backup@nextcloud_$date
+# Redémarrage du service Nextcloud sur le serveur
+ssh root@192.168.33.200 "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off"
--- backup_save.sh
+++ backup_save.sh
@@ -3,12 +3,20 @@
 # Nom bdd Nextcloud
 nc_bdd_name=nextcloud
 nc_bdd_user=admin
-nc_bdd_password=P@ssword
+nc_bdd_password=P@ssw0rd
 nc_bdd_host=localhost
 
 ssh root@192.168.33.200 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on'
 
-ssh root@192.168.33.200 "mysqldump --single-transaction -h localhost -u $nc_bdd_user -password=$nc_bdd_password $nc_bdd_name" > /data/backup/nextcloud_bdd.sql
+ssh root@192.168.33.200 "mysqldump --single-transaction -h localhost -u $nc_bdd_user -password=$nc_bdd_password $nc_bdd_name" > /data/backup/nextcloud_bdd.bak
 
-rsync -Aavx root@192.168.33.200:/var/www/html/nextcloud/ /data/backup/nextcloud-data/
+rsync -Aavx -e "ssh" root@192.168.33.200:/var/www/html/nextcloud/ /data/backup/nextcloud-data/
+zfs snapshot data/backup@nextcloud_`date +"%Y.%m.%d"`
+limite="data/backup@nextcloud_`date --date='-30 day' +"%Y.%m.%d"`"
+for snap in `zfs list -H -t snapshot -o name` ; do
+	if [[ $snap < $limite ]]; then
+		zfs destroy $snap
+	fi
+done
 
+ssh root@192.168.33.200 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off'
