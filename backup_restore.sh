bbcd84ba2a330e486e811a3771bebe98cf4a7b40
EnzoTUI <33655075+EnzoTUI@users.noreply.github.com>
2019-02-22 21:26:48 +0100

    Add files via upload
    new test

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-MYSQL_HOST=localhost
-MYSQL_USER=root
-MYSQL_PASS=root
-MYSQL_DBNAME=nextcloud
+#Varriables
+NomBDD=nextcloud
+Utilisateur=root
+pswd=root
+Host=root@192.168.33.200
 
-echo "Entrez la date de restauration voulu"
+
+echo "Renseignez la date de la restauration"
 read date
 
-# Arrêt du service Nextcloud sur le serveur
-ssh root@192.168.33.200 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on'
+# Mise en maintenance du service Nextcloud
+ssh $Host 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on'
 
-# Verrouillage de la snapshot
-# zfs hold keep data/backup@nextcloud_$date
-
-# Clone de la snapshot
+# Récupération du contenu de notre snapshot
 zfs clone data/backup@nextcloud_$date data/restore
 
-# Restauration des fichiers du serveur NextCloud
-rsync -Aavx /data/restore/nextcloud-data/ -e "ssh" root@192.168.33.200:/var/www/html/nextcloud/
-# Nettoyage de la BDD avant restauration
-ssh root@192.168.33.200  "mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASS -e \"DROP DATABASE $MYSQL_DBNAME\""
-ssh root@192.168.33.200  "mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASS -e \"CREATE DATABASE $MYSQL_DBNAME\""
+# Nettoyage de la base de données avant la restauration
+ssh $Host  "mysql -h localhost -u $Utilisateur --password=$pswd -e \"DROP DATABASE $NomBDD\""
+ssh $Host  "mysql -h localhost -u $Utilisateur --password=$pswd -e \"CREATE DATABASE $NomBDD\""
 
-# Restauration de la BDD du serveur NextCloud
-ssh root@192.168.33.200 "mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASS $MYSQL_DBNAME" < /data/restore/nextcloud-bdd.bak
+# Restauration des fichiers du service NextCloud
+rsync -Aavx /data/restore/nextcloud-data/ -e "ssh" $Host:/var/www/html/nextcloud/
 
-# Suppression du clone de la snapshot
+# Restauration de la base de données du service NextCloud
+ssh $Host "mysql -h localhost -u $Utilisateur --password=$pswd $NomBDD" < /data/restore/nextcloud_bdd.bak
+
+# Suppression du contenu de notre snapshot
 zfs destroy data/restore
-# Déverrouillage de la snapshot
-zfs release keep data/backup@nextcloud_$date
-# Redémarrage du service Nextcloud sur le serveur
-ssh root@192.168.33.200 "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off"
+
+# Arrêt de la maintenance et redémarrage du service Nextcloud
+ssh $Host "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off"
--- backup_save.sh
+++ backup_save.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Nom bdd Nextcloud
-nc_bdd_name=nextcloud
-nc_bdd_user=root
-nc_bdd_password=root
-nc_bdd_host=localhost
+#Varriables
+NomBDD=nextcloud
+Utilisateur=root
+pswd=root
+Host=root@192.168.33.200
 
-ssh root@192.168.33.200 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on'
 
-ssh root@192.168.33.200 "mysqldump --single-transaction -h localhost -u $nc_bdd_user --password=$nc_bdd_password $nc_bdd_name" > /data/backup/nextcloud_bdd.bak
+# Mise en maintenance du service Nextcloud
+ssh $Host 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on'
 
-rsync -Aavx -e "ssh" root@192.168.33.200:/var/www/html/nextcloud/ /data/backup/nextcloud-data/
+# Récupération des fichiers data du service Nextcloud
+rsync -Aavx -e "ssh" $Host:/var/www/html/nextcloud/ /data/backup/nextcloud-data/
+
+# Dump de la base de données mysql
+ssh $Host "mysqldump --single-transaction -h localhost -u $nc_bdd_user --password=$nc_bdd_password $nc_bdd_name" > /data/backup/nextcloud_bdd.bak
+
+# Réalisation d'une snapshot avec la date du jour dans son nom
 zfs snapshot data/backup@nextcloud_`date +"%Y.%m.%d"`
+
+# Suppression des snapshots de plus de 30 jours
 limite="data/backup@nextcloud_`date --date='-30 day' +"%Y.%m.%d"`"
 for snap in `zfs list -H -t snapshot -o name` ; do
 	if [[ $snap < $limite ]]; then
@@ -19,4 +27,7 @@ for snap in `zfs list -H -t snapshot -o name` ; do
 	fi
 done
 
-ssh root@192.168.33.200 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off'
+# Arrêt de la maintenance et redémarrage du service Nextcloud
+ssh $Host 'sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off'
+
+# Sauvegarde terminée
